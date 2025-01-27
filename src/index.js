6fa50aa15650bde34c54e5e308a420973ab8641c
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2025-01-27 20:51:38 +0200

    add js for burger-menu

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './sass/index.scss';
+
+import './js/burger-menu';
+// import './js/modal';
+// import './js/components/heroSlider';
+// import './js/components/last-public';
+// import './js/catalog/loadMorePoufs';
+// import './js/catalog/loadMoreTables';
+// import './js/openCard';
